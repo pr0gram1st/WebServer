34436847027c1f55058143d08c5698ae6db3b0b0
pr0gram1st <pr0gram1st@users.noreply.github.com>
2022-10-19 15:04:30 +0600

    Second Commit

--- src/main/java/org/example/Processor.java
+++ src/main/java/org/example/Processor.java
@@ -2,12 +2,13 @@ package org.example;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  * Processor of HTTP request.
  */
-public class Processor {
+public class Processor{
     private final Socket socket;
     private final HttpRequest request;
     public String response = "";
@@ -28,11 +29,42 @@ public class Processor {
         PrintFiles(arr, ++index, level);
     }
 
+    int toInt(String s){
+        int res = 0;
+        for(int i = 0; i < s.length(); ++i){
+            res *= 10;
+            res += (int)(s.charAt(i) - '0');
+        }
+        return res;
+    }
+
+    String sieve(int n){
+        String res = "";
+        int n1 = n + 100;
+        res += "Prime numbers between 1 and " + n + ": \n";
+        ArrayList<Boolean> prime=new ArrayList<>();
+        for(int i = 0; i <= n; ++i){
+            prime.add(true);
+        }
+        for(int i = 2; i * i <= n; ++i){
+            if(prime.get(i)){
+                for(int j = i * i; j <= n; j += i){
+                    prime.set(j, false);
+                }
+            }
+        }
+        for(int i = 2; i <= n; ++i){
+            if(prime.get(i)){
+                res += i + " ";
+            }
+        }
+        return res;
+    }
+
     public void process() throws IOException {
         System.out.println("Got request:");
         System.out.println(request.toString());
         System.out.flush();
-
         String s = request.toString();
         if(s.contains("create")){
             String name = "";
@@ -114,8 +146,22 @@ public class Processor {
                 PrintFiles(arr, 0, 0);
             }
         }
+        else if(s.contains("eratothenes_sieve")){
+            int pos = 7;
+            while(s.charAt(pos) != '/'){
+                pos++;
+            }
+            pos++;
+            String digit = "";
+            while(s.charAt(pos) != ' '){
+                char c = s.charAt(pos);
+                digit += c;
+                pos++;
+            }
+            int x = toInt(digit);
+            this.response += sieve(x);
+        }
         PrintWriter output = new PrintWriter(socket.getOutputStream());
-
         // We are returning a simple web page now.
         output.println("HTTP/1.1 200 OK");
         output.println("Content-Type: text/html; charset=utf-8");
@@ -125,6 +171,7 @@ public class Processor {
         output.println("<body><p>" + this.response + "</p></body>");
         output.println("</html>");
         output.flush();
+
         socket.close();
         this.response = "";
     }
